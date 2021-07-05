872f686a6419b6c180b5a6637116ec6fa2511c73
Victor Hermes <me.victorhermes@gmail.com>
2021-07-05 19:30:24 -0300

    feature: barchart started

--- components/StackedBarChartExemple/index.js
+++ components/StackedBarChartExemple/index.js
@@ -1,38 +1,63 @@
 import React from 'react';
-import { BarChart, YAxis } from 'react-native-svg-charts';
+import { BarChart, XAxis } from 'react-native-svg-charts';
 import { View, ScrollView } from 'react-native';
-import * as scale from 'd3-scale';
 
 export default function StackedBarChartExemple() {
-  const data = [14, 12, 30, 40, 2];
+  const data = [
+    {
+      value: 400,
+      label: 'Categoria',
+    },
+    {
+      value: 190,
+      label: 'Lazer',
+    },
+    {
+      value: 300,
+      label: 'Escola',
+    },
+    {
+      value: 200,
+      label: 'Educação',
+    },
+  ];
 
-  const width = data.length * 30;
+  const width = data.length * 50;
 
   return (
     <ScrollView horizontal>
       <View
         style={{
-          flexDirection: 'row',
           width: width,
-          height: 70,
-          marginTop: 20,
+          height: 140,
+          marginTop: 5,
+          marginLeft: 22,
         }}
       >
-        <YAxis
-          data={data}
-          yAccessor={({ index }) => index}
-          scale={scale.scaleBand}
-          contentInset={{ top: 10, bottom: 10 }}
-          spacing={0.2}
-          formatLabel={index => index}
-        />
         <BarChart
           style={{ flex: 1 }}
           data={data}
-          spacingInner={0.8}
+          spacingInner={0.85}
+          yAccessor={({ item }) => item.value}
           gridMin={0}
-          showGrid={true}
+          contentInset={{ top: 10, bottom: 0 }}
           svg={{ fill: '#bababa' }}
+          onPress={() => console.log('askd')}
+        />
+        <XAxis
+          style={{ marginHorizontal: -15, height: 70 }}
+          data={data}
+          contentInset={{ left: 24, right: 24 }}
+          svg={{
+            fontSize: 8,
+            fill: 'black',
+            rotation: 90,
+            originY: 7,
+            y: 4,
+            textAnchor: 'start',
+          }}
+          formatLabel={(_, index) => data[index].label}
+          yAccessor={({ index }) => index}
         />
       </View>
     </ScrollView>
